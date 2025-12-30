58e820f8eb7a83ffcd482d759c8f6f1e889a9d2f
kmora2450 <kmora2450@utm.edu.ec>
2025-12-29 22:56:05 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 import { createPool } from 'mysql2/promise'
 
 export const pool = createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1234567890',
-    database: 'tasksdb'
+    DB_HOST: 'localhost',
+    DB_PORT: 3306,
+    DB_USER: 'root',
+    DB_PASS: '1234567890',
+    DB_NAME: 'tasksdb'
 })
 
 
